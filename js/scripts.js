14a9ac4a9aaf124c8513851949868dd6b176f20b
Elan Baharie and Peter Karellas <peter@>
2017-04-12 08:44:52 -0700

    continue to refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,8 @@ Game.prototype.reqLib = function () {
 
     if(this.spot1 !== "Bourbon" || this.spot2 !== "Sweet Vermouth" || this.spot3 !== "Cherry") {
       return;
-    } else if (this.spot1 === "Bourbon" && this.spot2 === "Sweet Vermouth" && this.spot2 === "Cherry") {
-      this.bank + 10;
+    } else if (this.spot1 === "Bourbon" && this.spot2 === "Sweet Vermouth" && this.spot3 === "Cherry") {
+      this.bank = this.bank + 10;
       console.log("Victory Condition");
       return;
     }
@@ -41,7 +41,7 @@ Game.prototype.reqLib = function () {
       return;
     } else if (this.spot1 === "Tequila" && this.spot2 === "Triple Sec" && this.spot3 === "Lime Juice") {
       console.log("Victory Condition");
-      this.bank + 10;
+      this.bank = this.bank + 10;
       return;
     }
   }//End of Margarita Test
@@ -50,7 +50,7 @@ Game.prototype.reqLib = function () {
       return;
     } else if (this.spot1 === "Vodka" && this.spot2 === "Ginger Beer" && this.spot3 === "Lime Juice") {
     console.log("Victory Condition");
-      this.bank + 10;
+      this.bank = this.bank + 10;
       return;
     }
   }//End of Moscow Test
@@ -60,7 +60,7 @@ Game.prototype.reqLib = function () {
       return;
     } else if (this.spot1 === "Gin" && this.spot2 === "Dry Vermouth" && this.spot3 === "Twist") {
     console.log("Victory Condition");
-      this.bank + 10;
+      this.bank = this.bank + 10;
       return;
     }
   }//End of Gin Martini Test
@@ -70,7 +70,7 @@ Game.prototype.reqLib = function () {
       return;
     } else if (this.spot1 === "Vodka" && this.spot2 === "Dry Vermouth" && this.spot3 === "Twist") {
     console.log("Victory Condition");
-      this.bank + 10;
+      this.bank = this.bank + 10;
       return;
     }
   }//End of Vodka Martini Test
@@ -82,7 +82,7 @@ Game.prototype.reqLib = function () {
     } else if(this.spot1 === "" && this.spot2 === "" && this.spot3 === "Pinot Noir") {
       console.log("This is perfect");
       console.log("Victory Condition");
-      this.bank + 10;
+      this.bank = this.bank + 10;
       return;
     }
   }//End of Pinot Noir Test
@@ -92,7 +92,7 @@ Game.prototype.reqLib = function () {
     } else if(this.spot1 === "Gin" && this.spot2 === "" && this.spot3 === "Tonic") {
       console.log("This is perfect");
       console.log("Victory Condition");
-      this.bank + 10;
+      this.bank = this.bank + 10;
       return;
     }
   }//End of Gin and Tonic Test
@@ -104,13 +104,10 @@ Game.prototype.reqLib = function () {
     } else if(this.spot1 === "" && this.spot2 === "" && this.spot3 === "Chardonnay") {
       console.log("This is perfect");
       console.log("Victory Condition");
-      this.bank + 10;
+      this.bank = this.bank + 10;
       return;
     }
   }//End of Chardonnay Test
-  else if (this.bank === 0) {
-    return;
-  }
 
 
 
@@ -128,40 +125,6 @@ $(document).ready(function() {
     var spirVl = $(this).val();
     $("#firstSpot").val(spirVl);
   });
-  // $("#brbnBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#firstSpot").val("");
-  //   var spirVl = $("#brbnBtn").val();
-  //   $("#firstSpot").val(spirVl);
-  // });
-  // $("#ginBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#firstSpot").val("");
-  //   var spirVl = $("#ginBtn").val();
-  //   console.log(spirVl);
-  //   $("#firstSpot").val(spirVl);
-  // });
-  // $("#teqBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#firstSpot").val("");
-  //   var spirVl = $("#teqBtn").val();
-  //   console.log(spirVl);
-  //   $("#firstSpot").val(spirVl);
-  // });
-  // $("#vodBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#firstSpot").val("");
-  //   var spirVl = $("#vodBtn").val();
-  //   console.log(spirVl);
-  //   $("#firstSpot").val(spirVl);
-  // });
-  // $("#rumBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#firstSpot").val("");
-  //   var spirVl = $("#rumBtn").val();
-  //   console.log(spirVl);
-  //   $("#firstSpot").val(spirVl);
-  // });
 
 // below is column 2 mixers
   $(".secondary button").click(function(event){
@@ -171,35 +134,7 @@ $(document).ready(function() {
     console.log(mxVl);
     $("#secondSpot").val(mxVl);
   });
-  // $("#swtVrBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#secondSpot").val("");
-  //   var mxVl = $("#swtVrBtn").val();
-  //   console.log(mxVl);
-  //   $("#secondSpot").val(mxVl);
-  // });
-  //
-  // $("#trpSecBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#secondSpot").val("");
-  //   var mxVl = $("#trpSecBtn").val();
-  //   console.log(mxVl);
-  //   $("#secondSpot").val(mxVl);
-  // });
-  // $("#dryVrBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#secondSpot").val("");
-  //   var mxVl = $("#dryVrBtn").val();
-  //   console.log(mxVl);
-  //   $("#secondSpot").val(mxVl);
-  // });
-  // $("#gingBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#secondSpot").val("");
-  //   var mxVl = $("#gingBtn").val();
-  //   console.log(mxVl);
-  //   $("#secondSpot").val(mxVl);
-  // });
+
 // below is column 3 and 4
   $(".thirdFourth button").click(function(event){
     event.preventDefault();
@@ -208,65 +143,7 @@ $(document).ready(function() {
     console.log(altVl);
     $("#thirdSpot").val(altVl);
   });
-  // $("#chryBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#thirdSpot").val("");
-  //   var altVl = $("#chryBtn").val();
-  //   console.log(altVl);
-  //   $("#thirdSpot").val(altVl);
-  // });
-  // $("#limeJuiceBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#thirdSpot").val("");
-  //   var altVl = $("#limeJuiceBtn").val();
-  //   console.log(altVl);
-  //   $("#thirdSpot").val(altVl);
-  // });
-  // $("#twistBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#thirdSpot").val("");
-  //   var altVl = $("#twistBtn").val();
-  //   console.log(altVl);
-  //   $("#thirdSpot").val(altVl);
-  // });
-  // $("#tonicBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#thirdSpot").val("");
-  //   var altVl = $("#tonicBtn").val();
-  //   console.log(altVl);
-  //   $("#thirdSpot").val(altVl);
-  // });
-  //
-  //
-  // $("#pNBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#thirdSpot").val("");
-  //   var rdWnVl = $("#pNBtn").val();
-  //   console.log(rdWnVl);
-  //   $("#thirdSpot").val(rdWnVl);
-  // });
-  //
-  // $("#chardBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#thirdSpot").val("");
-  //   var rdWnVl = $("#chardBtn").val();
-  //   console.log(rdWnVl);
-  //   $("#thirdSpot").val(rdWnVl);
-  // });
-  // $("#beerBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#thirdSpot").val("");
-  //   var rdWnVl = $("#beerBtn").val();
-  //   console.log(rdWnVl);
-  //   $("#thirdSpot").val(rdWnVl);
-  // });
-  // $("#sodaBtn").click(function(event){
-  //   event.preventDefault();
-  //   $("#thirdSpot").val("");
-  //   var altVl = $("#sodaBtn").val();
-  //   console.log(altVl);
-  //   $("#thirdSpot").val(altVl);
-  // });
+
 //Player info submit
 
 $("#playerInfoForm").submit(function(event) {
@@ -299,8 +176,8 @@ $("#playerInfoForm").submit(function(event) {
   $("#guestBtn").click(function(event) {
     event.preventDefault();
     newGame.req = "";
-    console.log(newGame.req);
     newGame.req = newCustomer.createReq();
+    console.log(newGame.bank);
     console.log(newGame.req);
   });
 
